749df45ccdfe233ac4e1937ddcd6679f6d61a595
Brice Videau <brice.videau@imag.fr>
2014-07-07 15:01:50 +0200

    Fixed problem with 2D textures with OpenCL > 1.2

--- opencl-ruby-ffi/lib/opencl_ruby_ffi/opencl_ruby_ffi_base.rb
+++ opencl-ruby-ffi/lib/opencl_ruby_ffi/opencl_ruby_ffi_base.rb
@@ -134,7 +134,7 @@ module OpenCL
            :image_slice_pitch,    :size_t,
            :num_mip_levels,       :cl_uint, 
            :num_samples,          :cl_uint,  
-           :buffer,               Mem
+           :buffer,               :pointer
 
      # Creates anew ImageDesc using the values provided by the user
      def initialize( image_type, image_width, image_height, image_depth, image_array_size, image_row_pitch, image_slice_pitch, num_mip_levels, num_samples, buffer )
