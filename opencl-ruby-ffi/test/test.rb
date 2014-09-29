fce3e75ea67537a816be5c59f0162d640a4afaaa
Brice Videau <brice.videau@imag.fr>
2014-09-29 18:52:41 +0200

    Improved tests

--- opencl-ruby-ffi/test/test.rb
+++ opencl-ruby-ffi/test/test.rb
@@ -1,9 +1,9 @@
 require 'opencl_ruby_ffi'
 platforms = OpenCL::platforms
 source = <<EOF
-__kernel void addition(  float2 alpha, __global const float *x, __global float *y) {\n\
-  size_t ig = get_global_id(0);\n\
-  y[ig] = (alpha.s0 + alpha.s1 + x[ig])*0.3333333333333333333f;\n\
+__kernel void addition(  float2 alpha, __global const float *x, __global float *y) {
+  size_t ig = get_global_id(0);
+  y[ig] = (alpha.s0 + alpha.s1 + x[ig])*0.3333333333333333333f;
 }
 EOF
 
