f8e15b43ae90dc7bda6a157068717608e9673018
Brice Videau <brice.videau@imag.fr>
2015-05-22 13:47:44 +0200

    Added a second kernel to the test case.

--- opencl_ruby_ffi/test/test.rb
+++ opencl_ruby_ffi/test/test.rb
@@ -5,6 +5,10 @@ __kernel void addition(  float2 alpha, __global const float *x, __global float *
   size_t ig = get_global_id(0);
   y[ig] = (alpha.s0 + alpha.s1 + x[ig])*0.3333333333333333333f;
 }
+__kernel void sub(  float2 alpha, __global const float *x, __global float *y) {
+  size_t ig = get_global_id(0);
+  y[ig] = (alpha.s0 - alpha.s1 + x[ig])*0.3333333333333333333f;
+}
 EOF
 
 platforms.each { |platform|
