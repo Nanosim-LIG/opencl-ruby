7a67b4fa5c98d76821d8f53bc16bae748f7bbc2e
Brice Videau <brice.videau@imag.fr>
2015-05-27 15:55:35 +0200

    Added a tentative module to add a spir intermediary pass.

---
+++ opencl_ruby_ffi_spir/lib/opencl_ruby_ffi_spir.rb
@@ -0,0 +1,51 @@
+require 'opencl_ruby_ffi'
+require 'tempfile'
+
+$clang_path="clang"
+$clang_path = ENV["CLANG"] if ENV["CLANG"]
+$opencl_spir = "opencl_spir.h"
+$opencl_spir = ENV["OPENCL_SPIR_H"] if ENV["OPENCL_SPIR_H"]
+
+module OpenCL
+
+  class << self
+    alias :create_program_with_source_orig :create_program_with_source
+  end
+
+  def self.create_program_with_source(context, strings)
+    strs = nil
+    if not strings then
+      error_check(INVALID_VALUE)
+    else
+      strs = [strings].flatten
+    end
+    source_file = Tempfile::new(["openl_ruby_ffi_spir",".cl"])
+    path = source_file.path
+    strs.each { |s| source_file.puts s }
+    source_file.close
+    target = path.chomp(File::extname(path))+".spir"
+    devices = context.devices
+    `#{$clang_path} -cc1 -emit-llvm-bc -triple spir64-unknown-unknown -cl-std=CL1.2 -cl-spir-compile-options "-cl-std=CL1.2" -include #{$opencl_spir} -o #{target} #{source_file.path}`
+    binary = File::binread(target)
+    File::unlink(target)
+    binaries = []
+    devices.each { |d|
+      binaries.push binary
+    }
+    return create_program_with_binary(context, devices, binaries).first
+  end
+
+  class << self
+    alias :build_program_orig :build_program
+  end
+
+  def self.build_program(program, options = {}, &block)
+    opt = ""
+    opt = options[:options] if options[:options]
+    opt += "-x spir"
+    new_options = options.dup
+    new_options[:options] = opt
+    return build_program_orig(program, new_options, &block)
+  end
+
+end
---
+++ opencl_ruby_ffi_spir/test/test.rb
@@ -0,0 +1,34 @@
+[ '../lib', 'lib' ].each { |d| $:.unshift(d) if File::directory?(d) }
+require 'opencl_ruby_ffi_spir'
+platforms = OpenCL::platforms
+source = <<EOF
+__kernel void addition(  float2 alpha, __global const float *x, __global float *y) {
+  size_t ig = get_global_id(0);
+  y[ig] = (alpha.s0 + alpha.s1 + x[ig])*0.3333333333333333333f;
+}
+EOF
+platform = OpenCL::platforms.first
+device = platform.devices.first
+context = OpenCL::create_context(device)
+queue = context.create_command_queue(device, :properties => OpenCL::CommandQueue::PROFILING_ENABLE)
+prog = context.create_program_with_source( source )
+prog.build
+
+a_in = NArray.sfloat(65536).random(1.0)
+a_out = NArray.sfloat(65536)
+f = OpenCL::Float2::new(3.0,2.0)
+b_in = context.create_buffer(a_in.size * a_in.element_size, :flags => OpenCL::Mem::COPY_HOST_PTR, :host_ptr => a_in)
+b_out = context.create_buffer(a_out.size * a_out.element_size)
+event = prog.addition(queue, [65536], f, b_in, b_out, :local_work_size => [128])
+# k = prog.create_kernel("addition")
+# k.set_arg(0, f)
+# k.set_arg(1, b_in)
+# k.set_arg(2, b_out)
+# event = queue.enqueue_ndrange_kernel(k, [65536],:local_work_size => [128])
+queue.enqueue_read_buffer(b_out, a_out, :event_wait_list => [event])
+queue.finish
+diff = (a_in - a_out*3.0)
+65536.times { |i|
+  raise "Computation error #{i} : #{diff[i]+f.s0+f.s1}" if (diff[i]+f.s0+f.s1).abs > 0.00001
+}
+puts "Success!"
