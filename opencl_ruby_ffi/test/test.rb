dcb0c47ebdfe40fae133a2fc3a97fd8465ba9a71
Brice Videau <brice.videau@imag.fr>
2016-04-21 09:49:25 +0200

    Removed useless inspect.

--- opencl_ruby_ffi/test/test.rb
+++ opencl_ruby_ffi/test/test.rb
@@ -14,8 +14,8 @@ EOF
 
 platforms.each { |platform|
   puts platform.name + ": " + platform.version 
-  puts "version number #{platform.version_number.inspect}"
-  puts platform.extensions.inspect
+  puts "version number #{platform.version_number}"
+  p platform.extensions
   devices = platform.devices
   devices.each { |device|
     puts "  " + device.name
@@ -30,8 +30,8 @@ platforms.each { |platform|
   puts context.num_devices
   puts context.devices.first.name
   puts context.platform.name
-  puts OpenCL::Device::FPConfig::new(context.devices.first.double_fp_config).names.inspect
-  puts OpenCL::Device::Type::new(context.devices.first.type).names.inspect
+  p OpenCL::Device::FPConfig::new(context.devices.first.double_fp_config).names
+  p OpenCL::Device::Type::new(context.devices.first.type).names
   queue = context.create_command_queue(context.devices.first, :properties => OpenCL::CommandQueue::PROFILING_ENABLE)
   puts queue.properties
   a_in = NArray.sfloat(65536).random(1.0)
@@ -40,15 +40,14 @@ platforms.each { |platform|
   a_out[0] = 3.0
   a_out[1] = 2.0
   b_in = context.create_buffer(a_in.size * a_in.element_size, :flags => [OpenCL::Mem::COPY_HOST_PTR, OpenCL::Mem::READ_ONLY], :host_ptr => a_in)
-  puts b_in.inspect
-#  b_in = context.create_buffer(a_out.size * a_out.element_size)
+  p b_in
   b_out = context.create_buffer(a_out.size * a_out.element_size)
-  puts b_out.inspect
+  p b_out
   b_sub = b_in.create_sub_buffer( OpenCL::BUFFER_CREATE_TYPE_REGION, OpenCL::BufferRegion::new(0, b_in.size / 2 ) )
-  puts b_sub.inspect
+  p b_sub
   if context.devices.first.image_support == OpenCL::TRUE then
-    puts context.supported_image_formats( OpenCL::Mem::IMAGE2D ).inspect
-    puts context.create_image_2d( OpenCL::ImageFormat::new( OpenCL::ChannelOrder::RGBA, OpenCL::ChannelType::UNSIGNED_INT8), 128, 25).inspect
+    p context.supported_image_formats( OpenCL::Mem::IMAGE2D )
+    p context.create_image_2d( OpenCL::ImageFormat::new( OpenCL::ChannelOrder::RGBA, OpenCL::ChannelType::UNSIGNED_INT8), 128, 25)
   end
   puts b_in.size
   puts b_in.flags
@@ -63,24 +62,24 @@ platforms.each { |platform|
     puts "Finished building #{prog.inspect}! (data: #{data.inspect})"
   }
   sleep 1
-  puts prog.inspect
+  p prog
   puts prog.binary_sizes
-  puts prog.binaries.inspect
+  p prog.binaries
   puts prog.build_status
-  puts prog.build_log.inspect
+  p prog.build_log
   if prog.context.platform.version_number >= 1.2 then
     puts prog.kernel_names
-    prog.kernels.each { |k| puts k.inspect }
+    prog.kernels.each { |k| p k }
   end
   p2, st = OpenCL.create_program_with_binary(context, prog.devices, prog.binaries.collect { |d, b| b } )
   p2.build
-  puts p2.inspect
-  puts st.inspect
+  p p2
+  p st
 #  puts prog.binary_type.inspect
   k = prog.create_kernel("addition")
   puts k.function_name
   f = OpenCL::Float4::new(1,2,3,4)
-  puts f.inspect
+  p f
   puts f[:s2]
   puts f[:s3]
   puts f.alignment
@@ -93,16 +92,16 @@ platforms.each { |platform|
   e = queue.enqueue_NDrange_kernel(k, [65536],:local_work_size => [128])
 
   e = prog.addition(queue, [65536], f, b_in, b_out, :local_work_size => [128])
-  puts a_out.inspect
+  p a_out
   ek = queue.enqueue_read_buffer(b_out, a_out, :event_wait_list => [e])
-  puts ek.inspect
+  p ek
   #ek.set_callback(OpenCL::CommandExecutionStatus::COMPLETE, :user_data => a_in.to_ptr) { |ev, status, data| puts "Transfer finished! #{ev.to_ptr.inspect}, #{status}, #{data.inspect}" }
   queue.finish
   puts ek.command_execution_status
   puts ek.profiling_command_start
   puts ek.profiling_command_end
-  puts a_in.inspect
-  puts a_out.inspect
+  p a_in
+  p a_out
   diff = (a_in - a_out*3.0)
   65536.times { |i|
     raise "Computation error #{i} : #{diff[i]+f.s0+f.s1}" if (diff[i]+f.s0+f.s1).abs > 0.00001
