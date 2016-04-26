481e3aa8f745360ca3f6c5d05c0aebaeda990784
Brice Videau <brice.videau@imag.fr>
2016-04-26 18:10:56 +0200

    Indentation.

--- opencl_ruby_ffi/lib/opencl_ruby_ffi/Device.rb
+++ opencl_ruby_ffi/lib/opencl_ruby_ffi/Device.rb
@@ -26,7 +26,7 @@ module OpenCL
     error = clCreateSubDevices( in_device, props, device_number, devices_ptr, nil )
     error_check(error)
     devices_ptr.get_array_of_pointer(0, device_number).collect { |device_ptr|
-        Device::new(device_ptr, false)
+      Device::new(device_ptr, false)
     }
   end
 
